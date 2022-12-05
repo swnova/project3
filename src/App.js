a96ca5c3c79d6b3c6949217ed024cf46107f5df4
Sean Anthony <sammybanthony1@gmail.com>
2022-12-05 13:08:48 -0800

    header and stuff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import "./styles/App.css";
+import homepage from "./pages/homepage";
+import login from "./pages/login";
+import playpage from "./pages/playpage";
+import finalscore from "./pages/finalscore";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <p>"hello"</p>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/playpage" element={<Playpage />} />
+        <Route path="/finalscore" element={<Finalscore />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ src/pages/login/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Log In() {
+  return (
+    <div>Log In</div>
+  )
+}
