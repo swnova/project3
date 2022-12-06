ef8b1df5b9bd5261ef37de65fdd51a369917d4b1
Sean Anthony <sammybanthony1@gmail.com>
2022-12-06 12:25:12 -0800

    some changes

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
 
 export default function Homepage() {
   return (
-    <div>Homepage</div>
-
-    
-  )
+    <div>
+      Homepage
+      <div class="flex flex-col justify-center items-center w-full h-full p-5">
+        <div class="homepage border">
+          <form class="flex flex-col justify-center items-center m-5">
+            <div class="mt-4">
+              <button class="bg-sky-500 p-2" id="singlePlayerButton">
+                {" "}
+                Single Player
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div class="flex flex-col justify-center items-center w-full h-full p-5">
+        <div class="homepage border">
+          <form class="flex flex-col justify-center items-center m-5">
+            <div class="mt-4">
+              <button class="bg-sky-500 p-2" id="mulitPlayerButton">
+                {" "}
+                Multi Player
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
