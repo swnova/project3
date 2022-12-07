43fed47051cc2a9ee50d8de9d2bc85da3551490f
swnova <sharrodwillanova@gmail.com>
2022-12-06 23:47:52 -0800

    adding boilerplate code for server graphql

---
+++ server/models/index.js
@@ -0,0 +1,3 @@
+const Player = require('./player');
+
+module.exports = {Player};
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Footer.css";
+
+function Footer() {
+  return (
+    <div>Footer</div>
+  );
+}
+
+
+export default Footer;
