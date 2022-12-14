9a152cefb959aa18b7a7ea97bee99d73e5ace0bd
Artem Khomenko <artem011202@gmail.com>
2022-12-14 15:17:56 -0800

    add footer

---
+++ src/Components/footer/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./footer.css"
+
+const Footer = () => (
+  <div className="footer">
+    <p>All customer email and credit card information is subject to resale via the Chinese government</p>
+  </div>
+);
+
+export default Footer;
